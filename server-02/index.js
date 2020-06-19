52c82588f3941b8b75981115b23db9d6cd69d3fd
Petr Volf <petr.volf96@gmail.com>
2020-06-19 11:28:02 +0200

    rolling update

--- server-02/index.js
+++ server-02/index.js
@@ -2,13 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let ready = false
+
+
 app.get('/', (req, res) => {
     console.log('incoming request')
-    return res.send('Hello from Kubernetes!')
+    return res.send('Hello from updated Kubernetes!')
 })
 
 app.get('/ready', (req, res) => {
     console.log('incoming ready request')
+    if (!ready) {
+        setTimeout(() => {
+            ready = true
+        }, 10000)
+        return res.status(400).send()
+    }
     return res.status(200).send()
 })
 
